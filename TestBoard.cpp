260cb5fec2e216bd20986a446946ec59e743d3e1
Ivan Rao <ivanrao@Ivans-MacBook-Air.local>
2021-11-19 15:55:15 -0500

    added makefile and TestBoard.cpp to test the game and functions

---
+++ TestBoard.cpp
@@ -0,0 +1,11 @@
+//File name: /Users/laptopuser/Documents/cs135_codes/projects/game1024/TestBoard.cpp
+#include <iostream>
+#include "Board.hpp"
+using namespace std;
+
+int main()
+{
+    Board game(4);
+    //game.start();
+    return 0;
+}
